7681f535ab0c28317ba596bb88f55e4a7192ec3e
Rasmus Gustafsson <rasmus.gustafsson2611@gmail.com>
2023-07-01 01:51:05 +0300

    perf: add basic query benchmarks

--- bench/crud_bench.ts
+++ bench/crud_bench.ts
@@ -1,13 +1,55 @@
-// import { add } from "../main.ts";
+import {
+  clearMocks,
+  createMockDatabase,
+  populateMockDatabase,
+} from "../test/util.ts";
 
-function add(a: number, b: number) {
-  return a + b;
-}
+const db = await createMockDatabase();
+await clearMocks();
+await populateMockDatabase(db);
 
-Deno.bench(function addSmall() {
-  add(1, 2);
+const mockUser = {
+  id: "07c2c2bc-0513-4fe5-8b4a-70e8f73cdde6",
+  createdAt: new Date(1337),
+  name: "John Doee",
+};
+
+const createMockItem = () => ({
+  id: crypto.randomUUID(),
+  createdAt: new Date(),
+  name: "Double Cheeseburger",
+  userId: mockUser.id,
 });
 
-Deno.bench(function addBig() {
-  add(2 ** 32, 2 ** 32);
+// A lot of double cheeseburgers
+async function createManyOrders() {
+  const orders = [...new Array(10_000).keys()].map(() => createMockItem());
+  for (let i = 0; i < orders.length; i++) {
+    await db.orders.create({
+      data: orders[i],
+    });
+  }
+  console.log(`🍔 Created ${orders.length} mock double cheeseburger orders.`);
+}
+
+await createManyOrders();
+
+Deno.bench("findFirst > where", async () => {
+  await db.users.findFirst({
+    where: {
+      id: "67218087-d9a8-4a57-b058-adc01f179ff9",
+    },
+  });
+});
+
+Deno.bench("findMany", async () => {
+  await db.users.findMany({});
+});
+
+Deno.bench("findMany > where", async () => {
+  await db.users.findMany({
+    where: {
+      name: "Double Cheeseburger",
+    },
+  });
 });
