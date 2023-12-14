72a7254ae21c5b6abb414d20898eb130061bda56
Maarten Bicknese <maarten.bicknese@gmail.com>
2023-12-14 21:23:03 +0100

    chore(demo): remove boilerplate code

--- examples/demo/fresh.gen.ts
+++ examples/demo/fresh.gen.ts
@@ -2,25 +2,19 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $_name_ from "./routes/[name].tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $_middleware from "./routes/_middleware.ts";
-import * as $api_joke from "./routes/api/joke.ts";
 import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
 import * as $SubmitTask from "./islands/SubmitTask.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $_name_,
     "./routes/_app.tsx": $_app,
     "./routes/_middleware.ts": $_middleware,
-    "./routes/api/joke.ts": $api_joke,
     "./routes/index.tsx": $index,
   },
   islands: {
-    "./islands/Counter.tsx": $Counter,
     "./islands/SubmitTask.tsx": $SubmitTask,
   },
   baseUrl: import.meta.url,
